f2bde11bec6d0cbf109d84dd828a49d44bbcf13d
Gio Cornejo - Local <giocornejo777@gmail.com>
2023-12-06 11:37:55 +0800

    Fixed encapsulation error

--- models/encryption_decryption_base.py
+++ models/encryption_decryption_base.py
@@ -8,13 +8,13 @@ class EncryptionBase(ABC):
 
     def perform_encryption(self, code: str) -> bool:
         encrypted_result = self.encrypt(code)
-        print(f"{encryption_result_message} {encrypted_result.__message}")
-        return encrypted_result.__is_passed
+        print(f"{encryption_result_message} {encrypted_result.message()}")
+        return encrypted_result.is_passed()
 
     def perform_decryption(self, code: str) -> bool:
         decrypted_result = self.decrypt(code)
-        print(f"{decryption_result_message} {decrypted_result.__message}")
-        return decrypted_result.__is_passed
+        print(f"{decryption_result_message} {decrypted_result.message()}")
+        return decrypted_result.is_passed()
 
     @abstractmethod
     def encrypt(self, code: str) -> Result:

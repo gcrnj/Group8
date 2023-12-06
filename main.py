00745d56860e9b8c399059141a2e1c6b336e32ab
Gio Cornejo - Local <giocornejo777@gmail.com>
2023-12-06 11:35:03 +0800

    Removed unnecessary function. Applied encapsulation

--- main.py
+++ main.py
@@ -1,58 +1,5 @@
-import sys
-from models.polybius import Polybius
+from Group8Project import Group8Project
 from constants import *
-from models.rot13 import Rot13
-
-
-class Group8Project:
-
-    def __init__(self):
-        self.rot13 = Rot13()
-        self.polybius = Polybius()
-
-    def __exit_all(self):
-        print(goodbyeMessage)
-        sys.exit()
-
-    def get_user_selection(self) -> Topic:
-        selected = Topic.Invalid
-        while selected == Topic.Invalid:
-            userSelection = input(
-                f"{userSelectionQuestion} {newLine}"
-                f"{rot13EncryptionOption} {newLine}"
-                f"{rot13DecryptionOption} {newLine}"
-                f"{polybiusEncryptionOption} {newLine}"
-                f"{polybiusDecryptionOption} {newLine}"
-                f"{exitOption} {newLine}"
-                # f"{divider}"
-                f"{selectionText}"
-            ).upper()  # Convert to uppercase so that the input is NOT case-sensitive
-            isValidTopicsOption = any(userSelection == topic.value for topic in Topic)
-            if isValidTopicsOption and userSelection != Topic.Exit.value and userSelection != Topic.Invalid.value:
-                selected = Topic.find_member_by_value(userSelection)
-            elif userSelection == Topic.Exit.value:  # Exit
-                self.__exit_all()
-            else:  # Invalid
-                print(invalidOption)
-                print((divider * 2).strip())
-                selected = Topic.Invalid
-        return selected
-
-    # Rot 13 - Encrypt
-    def encrypt_rot13(self) -> bool:
-        return self.rot13.perform_encryption(input("Decrypted code > "))
-
-    # Rot 13 - Decrypt
-    def decrypt_rot13(self) -> bool:
-        return self.rot13.perform_decryption(input("Encrypted Code > "))
-
-    # Polybius - Encrypt
-    def encrypt_polybius(self) -> bool:
-        return self.polybius.perform_encryption(input("Decrypted code > "))
-
-    # Polybius - Decrypt
-    def decrypt_polybius(self) -> bool:
-        return self.polybius.perform_decryption(input("Encrypted code > "))
 
 
 if __name__ == '__main__':
--- models/encryption_decryption_result.py
+++ models/encryption_decryption_result.py
@@ -2,8 +2,14 @@ class Result:
     # [is_passed] - If there is not error.
     # [message] - is the encrypted, decrypted, or error message depending on the value of [is_passed].
     def __init__(self, is_passed: bool, message: str):
-        self.is_passed = is_passed
-        self.message = message
+        self.__is_passed = is_passed
+        self.__message = message
+
+    def is_passed(self):
+        return self.__is_passed
+
+    def message(self):
+        return self.__message
 
     def __str__(self):
-        return f"User(username={self.is_passed}, email={self.message})"
+        return f"User(username={self.__is_passed}, email={self.__message})"
